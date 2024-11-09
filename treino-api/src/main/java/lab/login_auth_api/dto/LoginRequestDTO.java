ceb7f484a2e047bd5651ff99b8d2626c040c274a
Antony Guilherme <83239872+antonycabral@users.noreply.github.com>
2024-11-09 12:53:11 -0300

    fix

--- treino-api/src/main/java/lab/login_auth_api/dto/LoginRequestDTO.java
+++
@@ -1,5 +0,0 @@
-package lab.login_auth_api.dto;
-
-public record LoginRequestDTO(String email, String password) {
-
-}
--- treino-api/src/main/java/lab/login_auth_api/dto/RegisterRequestDTO.java
+++
@@ -1,5 +0,0 @@
-package lab.login_auth_api.dto;
-
-public record RegisterRequestDTO(String name, String email, String password) {
-
-}
